6b96a3078566d70603e7c64110faff51af2d867a
Jongwoo Choi <muvnelik@naver.com>
2023-11-27 18:27:23 +0900

    #21 - 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용
    테이블 필드 수정으로 영향받은
    'data.sql' 테스트 데이터도 모두 업데이트
    정렬을 최신순으로 적용

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -8,7 +8,8 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
+
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
@@ -27,19 +28,25 @@ public class ArticleComment extends AuditingFields {
     private Article article;
 
     @Setter
+    @ManyToOne(optional = false)
+    private UserAccount userAccount; // 유저 정보 (ID)
+
+
+    @Setter
     @Column(nullable = false, length = 500)
     private String content; // 본문
 
     protected ArticleComment() {
     } // @NoArgConstructor 임
 
-    private ArticleComment(Article article, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article, content);
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, content);
     }
 
     @Override
