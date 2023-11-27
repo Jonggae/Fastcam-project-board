c026afa91816af9e9f79e522b86ab3202ef2fc7e
Jongwoo Choi <muvnelik@naver.com>
2023-11-27 17:51:38 +0900

    #18 임시 커밋
    이번만 세세한 커밋 없이지나감. 보완해야 할 것이 있음.

---
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/dto/ArticleCommentDto.java
@@ -0,0 +1,14 @@
+package com.fastcampus.projectboard.dto;
+
+import java.time.LocalDateTime;
+
+public record ArticleCommentDto(
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy,
+        String content) {
+    public static ArticleCommentDto of(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String content) {
+        return new ArticleCommentDto(createdAt, createdBy, modifiedAt, modifiedBy, content);
+    }
+}
